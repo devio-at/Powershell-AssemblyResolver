41f0c0f06c6609c6b98e633423d264a62b911c43
devio-at <github@devio.at>
2020-01-05 15:08:14 +0100

    initial commit

---
+++ AssemblyResolver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace devio.Powershell
+{
+	// this code was originally posted at
+	// http://stackoverflow.com/questions/2664028/how-can-i-get-powershell-added-types-to-use-added-types/2669782#2669782
+	// I added the AddAssemblyPath method and the assembly lookup added paths
+	// published under license for StackOverflow/StackExchange
+	// CC BY-SA http://creativecommons.org/licenses/by-sa/3.0/
+
+	public class AssemblyResolver
+	{
+		private static Dictionary<string, string> _assemblies;
+		private static List<string> paths;
+
+		static AssemblyResolver()
+		{
+			var comparer = StringComparer.CurrentCultureIgnoreCase;
+			_assemblies = new Dictionary<string, string>(comparer);
+			paths = new List<string>();
+			AppDomain.CurrentDomain.AssemblyResolve += ResolveHandler;
+		}
+
+		public static void AddAssemblyLocation(string path)
+		{
+			// This should be made threadsafe for production use
+			string name = Path.GetFileNameWithoutExtension(path);
+			_assemblies.Add(name, path);
+		}
+
+		public static void AddAssemblyPath(string path)
+		{
+			paths.Add(path);
+		}
+
+		private static Assembly ResolveHandler(object sender, ResolveEventArgs args)
+		{
+			var assemblyName = new AssemblyName(args.Name);
+			if (_assemblies.ContainsKey(assemblyName.Name) && File.Exists(_assemblies[assemblyName.Name]))
+			{
+				return Assembly.LoadFrom(_assemblies[assemblyName.Name]);
+			}
+			foreach (var path in paths)
+			{
+				var filename = Path.Combine(path, assemblyName.Name) + ".dll";
+				if (File.Exists(filename))
+					return Assembly.LoadFrom(filename);
+			}
+			return null;
+		}
+	}
+}
---
+++ load-assembly.ps1
@@ -0,0 +1,47 @@
+﻿
+# adjust paths:
+
+$resolver = $pwd.Path + "\devio.Powershell.dll"
+
+$filename = $pwd.Path + "\My.Assembly.dll"
+
+#
+# use the dll's config to retrieve nhibernate settings and connection string
+#
+# http://stackoverflow.com/questions/835862/powershell-calling-net-assembly-that-uses-app-config
+#
+[System.AppDomain]::CurrentDomain.SetData("APP_CONFIG_FILE", $filename + ".config")
+
+#
+# http://msdn.microsoft.com/en-us/library/system.appdomain.getdata.aspx
+#
+[System.AppDomain]::CurrentDomain.SetData("APPBASE", [System.IO.Path]::GetDirectoryName( $filename ) + "\")
+[System.AppDomain]::CurrentDomain.SetData("PRIVATE_BINPATH", [System.IO.Path]::GetDirectoryName( $filename ) + "\")
+
+[System.Environment]::CurrentDirectory = $pwd.Path
+
+
+# http://stackoverflow.com/questions/2664028/how-can-i-get-powershell-added-types-to-use-added-types
+
+$dummy = [System.Reflection.Assembly]::LoadFrom($resolver)
+[devio.Powershell.AssemblyResolver]::AddAssemblyPath( [System.IO.Path]::GetDirectoryName( $filename ) )
+
+$dummy = [System.Reflection.Assembly]::LoadFrom($filename)
+
+"assembly loaded"
+
+trap 
+{ 
+"<<<<<"
+
+	$error[0].Exception.ToString() 
+
+">>>>>"
+}
+
+
+$object = new-object My.Assembly.MyClass
+
+# list all loaded assemblies:
+# [System.AppDomain]::CurrentDomain.GetAssemblies() | select-object  FullName
+
